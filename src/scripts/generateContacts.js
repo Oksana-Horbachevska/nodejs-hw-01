67e8490786af5373a1d1b520d0aae3fb927f2b0f
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-09-06 18:45:29 +0200

    first commit HW-1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+
+  for (let index = 0; index < number; index++) {
+    contacts.push(createFakeContact());
+  }
+  await writeContacts(contacts);
+};
 
 generateContacts(5);
